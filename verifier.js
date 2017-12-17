1023236bf22bba2447ddc4067ed44641a98034c1
Arash T.C <Captainarash@users.noreply.github.com>
2017-12-17 12:13:33 +0200

    Added Verification Methods
    -- Now can verify function names
    -- and partly verify the body syntax of a function.
    
    ...more to come.

--- verifier.js
+++ verifier.js
@@ -9,19 +9,92 @@ var keywords = ['auto','double','int',
 var dataTypes = ['int','char','float','double','void'];
 
 function verifier(foundFuncs) {
+  var names = [];
+  for (var i = 0; i < foundFuncs.length; i++) {
+    names.push(foundFuncs[i].name);
+  }
+
+  if (!verifyFunctionNames(names)) {
+    throw new TypeError('error in function definition: duplicate or illigal name!');
+  }
+
   var current = 0;
   while (current < foundFuncs.length) {
     var func = foundFuncs[current];
+
     if (!verifyReturnType(func.returnType)) {
-      throw new TypeError('return Type error in function definition for the function: ' + func.name);
+      throw new TypeError('returnType error in function definition for the function: ' + func.name);
       break;
     }
+
     if (!verifyFunctionArguments(func.args)) {
       throw new TypeError('Error in function definition: Invalid Arguments!');
       break;
     }
+
+    if (!verifyFunctionBody(func.body)) {
+      throw new TypeError('Error! Function Body (Statements) are screwed up!');
+      break;
+    }
+
     current++;
   }
+}
+
+
+function verifyFunctionBody(funcBody) {
+  var current = 0;
+  while (current < funcBody.length) {
+    var part = funcBody[current];
+    if (part.type === 'Statement') {
+      var smallPart = part.value;
+      if (smallPart[smallPart.length - 1].type !== 'Terminator') {
+        return 0;
+      }
+      for (var i = 0; i < smallPart.length; i++) {
+        if (smallPart[i].value === 'return') {
+          if (i !== 0) {
+            return 0;
+          }
+        }
+      }
+    }
+
+    if (part.type === 'if') {
+      var cond = part.condition;
+      if (cond.length === 1) {
+        if (cond[0].type === 'Word') {
+          if (keywords.indexOf(cond[0].value) !== -1) {
+            return 0;
+          }
+        } else if (cond[0].type === 'NumberLiteral') {
+            current++;
+            continue;
+        }
+      }
+    }
+    current++;
+  }
+  return 1;
+}
+
+function verifyFunctionNames(funcNames) {
+  for (var i = 0; i < funcNames.length; i++) {
+    if (keywords.indexOf(funcNames[i]) !== -1) {
+      return 0;
+    }
+  }
+  var sortedFuncNames = funcNames.slice().sort();
+  var duplicates = [];
+  for (var i = 0; i < sortedFuncNames.length - 1; i++) {
+      if (sortedFuncNames[i + 1] == sortedFuncNames[i]) {
+          duplicates.push(sortedFuncNames[i]);
+      }
+  }
+  if (duplicates.length !== 0) {
+    return 0;
+  }
+  return 1;
 }
 
 function verifyReturnType(returnType) {
